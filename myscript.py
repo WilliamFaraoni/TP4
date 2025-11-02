d06f92d88b5d8497cba5b737564a71af1316a6e5
William Faraoni <wilfaraoni@gmail.com>
2025-11-02 15:21:45 -0500

    patie 3-3

--- myscript.py
+++ myscript.py
@@ -1,6 +1,6 @@
 import os
 bad_hash = os.environ.get("GITHUB_SHA")
-good_hash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c)"
+good_hash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 
 os.system(f"git bisect start {bad_hash} {good_hash}")
 os.system("git bisect run python manage.py test")
